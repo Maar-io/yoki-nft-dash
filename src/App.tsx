68de1d26865ecfed83566e992b253bc36c688782
Mar.io <34627453+Maar-io@users.noreply.github.com>
2024-03-09 20:21:13 +0100

    added favicon and logo

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import FetchNfts from './Components/FetchNfts';
 import YokiSum from './Components/YokiSum';
 import PartnerNftsContext from './Components/PartnerNftsContext';
+import logo from './logo.png'; // path to your logo file
 
 function App() {
   const [partnerNfts, setPartnerNfts] = useState(null);
@@ -12,7 +13,7 @@ function App() {
     <>
       <div className="App">
         <header className="App-header">
-          <p>
+        <img src={logo} alt="Logo" style={{ position: 'absolute', top: 0, left: 0, width: '100px', height: '100px' }} />          <p>
             Yoki Origins NFT dashboard
           </p>
         </header>
