cba13e1a24ec216ff3bf5d9045fba35b060c1399
Mar.io <34627453+Maar-io@users.noreply.github.com>
2024-03-09 13:15:42 +0100

    added contracct read

--- src/Components/FetchNfts.tsx
+++ src/Components/FetchNfts.tsx
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import NftTable from './NftTable';
 import { NftData } from './types'; // adjust the path as needed
+import { ethers } from 'ethers';
 
+const RPC = 'https://rpc.startale.com/astar-zkevm';
+
+const contractABI = [
+  "function totalSupply() view returns (uint256)"
+];
+
+const provider = new ethers.providers.JsonRpcProvider(RPC);
 
 const FetchNfts: React.FC = () => {
   const [rows, setRows] = useState<NftData[]>([]);
@@ -9,19 +17,21 @@ const FetchNfts: React.FC = () => {
   useEffect(() => {
     fetch('/nfts.json')
       .then(response => response.json())
-      .then(data => {
-        const updatedData = data.map((item: NftData) => ({
-          ...item,
-          minted: Math.floor(Math.random() * 1000), 
+      .then(async data => {
+        const updatedData = await Promise.all(data.map(async (item: NftData) => {
+          const contract = new ethers.Contract(item.contract, contractABI, provider);
+          const minted = await contract.totalSupply();
+          return { ...item, minted: minted.toNumber() };
         }));
         setRows(updatedData);
       });
   }, []);
 
-    return (
-        <div className="nft-table-container">
+  return (
+    <div className="nft-table-container">
       <NftTable data={rows} />
-      </div>    );
+    </div>
+  );
 };
 
 export default FetchNfts;
